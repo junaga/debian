bfef3bd0989dde894c0414123cab536fdf62b61c
junaga <hermann-stanew@invita.gmbh>
2021-08-28 20:38:00 +0200

    Add custom bash `PS1`

--- junaga.sh
+++ junaga.sh
@@ -12,3 +12,10 @@ function commit-cmd {
 
 # rename the master branch of an empty repo created with `git init`
 alias main-lives-matter='git symbolic-ref HEAD refs/heads/main'
+
+# Hacked together bash shell prompt.
+# ANSI Escape Sequences https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+# Bash prompt https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Controlling-the-Prompt
+
+# "HH:MM" + " " + "ESC[Bold;Blue" + "PWD" + "ESC[Reset" + "Git branch" + "$ "
+export PS1="\A \e[1;34m\w\e[0m\$(__git_ps1 '[%s]')$ "
