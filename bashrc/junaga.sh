8e1e70523116efff4342909d0a7b72b9ed8614a6
junaga <hermann-stanew@invita.gmbh>
2022-03-25 10:06:25 +0100

    fmt: junaga.sh

--- bashrc/junaga.sh
+++ bashrc/junaga.sh
@@ -16,6 +16,6 @@ alias main-lives-matter='git symbolic-ref HEAD refs/heads/main'
 # Hacked together bash shell prompt.
 # ANSI Escape Sequences https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
 # Bash prompt https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Controlling-the-Prompt
-
+#
 # "HH:MM" + " " + "ESC[Bold;Blue" + "PWD" + "ESC[Reset" + "Git branch" + "$ "
 export PS1="\A \e[1;34m\w\e[0m\$(__git_ps1 '[%s]')$ "
