cdd2a536ce98ce628bd87de736d3974f52c8cb11
junaga <hermann-stanew@invita.gmbh>
2022-04-09 23:21:34 +0200

    Refactor `junaga.sh`

--- bashrc/junaga.sh
+++ bashrc/junaga.sh
@@ -1,21 +1,28 @@
-alias gitCommitAll="git add ./ && git commit"
-alias gitAmendAll="git add ./ && git commit --amend --no-edit"
+# Hacked together bash shell prompt.
+#
+# ANSI Escape Sequences https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+# Bash prompt https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Controlling-the-Prompt
+# "HH:MM" + " " + "ESC[Bold;Blue" + "PWD" + "ESC[Reset" + "Git branch" + "$ "
+export PS1="\A \e[1;34m\w\e[0m\$(__git_ps1 '[%s]')$ "
 
-# run a command, commit all files, the commit msg is the command wrapped in ``$ MSG``
-function gitCommitCmd {
-  cmd=$1
-
-  eval $cmd &&
-  git add ./ &&
-  git commit -m "\`$ $cmd\`"
-}
+# Configure Bash glob expansion
+shopt -s dotglob globstar failglob
+GLOBIGNORE='.:..'
 
 # rename the master branch of an empty repo created with `git init`
 alias main-lives-matter='git symbolic-ref HEAD refs/heads/main'
 
-# Hacked together bash shell prompt.
-# ANSI Escape Sequences https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
-# Bash prompt https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Controlling-the-Prompt
-#
-# "HH:MM" + " " + "ESC[Bold;Blue" + "PWD" + "ESC[Reset" + "Git branch" + "$ "
-export PS1="\A \e[1;34m\w\e[0m\$(__git_ps1 '[%s]')$ "
+# Quickly run the firebase CLI
+alias fire='npx firebase-tools --project $FIREBASE_PROJECT'
+
+# `apt show` the package of a binary in `$PATH``
+function show-pkg {
+  path=$(which "$1")
+  pkg=$(dpkg -S "$path" | sed 's/: .*//')
+  apt show "$pkg"
+}
+# list the binaries placed in `$PATH` from an installed package
+function list-bin {
+  pkg="$1"
+  dpkg -L "$pkg" | grep -E "$(echo $PATH | tr ':' '|')"
+}
