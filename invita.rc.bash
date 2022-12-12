62686359a24f52d94f159cd23fbb14d97d69e8fd
junaga <hermann-stanew@invita.gmbh>
2022-12-12 05:16:52 +0100

    fix: vscode injection overwrote $PROMPT_COMMAND

--- invita.rc.bash
+++ invita.rc.bash
@@ -8,11 +8,11 @@ set +h
 # sensible ~/.bash_history settings
 HISTIGNORE="export *"
 shopt -s histappend
-PROMPT_COMMAND="history -a && history -n"
+PROMPT_COMMAND="history -a; history -n"
 HISTSIZE=-1
 HISTFILESIZE=-1
 
-# shellcheck disable=SC1091 
+# shellcheck disable=SC1091
 source /usr/share/bash-completion/bash_completion
 
 alias ls='ls --color=auto -h'
@@ -24,6 +24,9 @@ export EDITOR="code --wait" # VS Code FTW
 # unset WINDOW_MANAGER
 # export BROWSER="chrome" # everything is chrome in the future
 
+prev=$PROMPT_COMMAND
 # only required when App setting "terminal.integrated.shellIntegration.enabled" is false.
 # shellcheck disable=SC1090
 source "$(code --locate-shell-integration-path bash)"
+PROMPT_COMMAND="$prev; $PROMPT_COMMAND"
+unset prev
