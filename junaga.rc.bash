a723a4ea9f8a330a116c02db0996510a9d7c1c4f
junaga <hermann-stanew@invita.gmbh>
2022-12-12 02:23:45 +0100

    Custom $PS1 shell prompt

--- junaga.rc.bash
+++ junaga.rc.bash
@@ -1,3 +1,25 @@
+##### Customized Prompt #####
+export PATH=$PATH:$HOME/bin
+set -C # redirection (`>`) overwites' error out
+
+# Terminal, ANSI Escape Code, Control Sequence Introducer, Select Graphic Rendition, Parameters (or simply ANSI color codes)
+# https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_(Select_Graphic_Rendition)_parameters
+#
+# ANSI Color codes syntax
+# "\[\e[" + $CODE + "m\]" + $TEXT + "\[\e[m\]"
+# where $CODE is one or multiple color codes separated by ";"
+# and $TEXT is the text to be colored.
+# The closing sequence "\[\e[m\]" resets all color codes.
+#
+# ANSI Escape codes are parsed by the terminal emulator not the shell.
+# So the shell won't misscount printed characters (line wrapping issues),
+# escape the color codes with "\[" "\]"
+# Otherwise the shell and terminal cursor position will get out of sync.
+
+# HH:MM+" "+BLUE;BOLD+PWD+RESET+<"|"+GIT_BRANCH>+"$"+" "
+PS1="\A \[\e[34;1m\]\w\[\e[m\]\$(__git_ps1 '|%s')$ "
+PS2="  "
+
 ##### Aliases #####
 alias time='date +"%Y-%m-%dT%H:%M:%S%:z"'
 alias rm="trash-put"
