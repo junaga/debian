577bbc5ae96a1879d987e541c5bc5d876b5816fe
Hermann Stanew <hermann-stanew@invita.gmbh>
2023-02-01 01:30:30 +0100

    fmt

--- invita.rc.bash
+++ invita.rc.bash
@@ -1,14 +1,13 @@
 echo "Welcome $USER"
 trap 'echo "Goodbye $USER, now lvl $((SHLVL-1))"' EXIT
 
-##### Fix the Shell #####
-# disable in-memory command hashmap, recrawl $PATH on every command entered
+##### Fix the shell #####
+# don't cache binary locations. search $PATH on every command entered
 set +h
-# keep history of all commands in-memory (not just the last 500) entered
-HISTSIZE=-1
 
-# show me someone with a b&w CRT monitor
-# nvm: https://www.reddit.com/r/crtgaming/comments/u2nbu4/may_i_present_you_this_tiny_bw_crt_its_only_5/
+# send ANSI color codes to the terminal
+# # show me someone with a b&w CRT monitor
+# # nvm: https://www.reddit.com/r/crtgaming/comments/u2nbu4/may_i_present_you_this_tiny_bw_crt_its_only_5/
 alias ls='ls --color=auto -h'
 alias grep='grep --color=auto'
 
@@ -19,8 +18,9 @@ source /usr/share/bash-completion/bash_completion
 # https://manpages.debian.org/bullseye/bash/bash.1.en.html#HISTORY
 
 HISTIGNORE="export *" # but keep secrets secret
+HISTSIZE=-1 # keep history of all commands (not just 500) entered
 shopt -s histappend # append to the history file, don't overwrite it
-PROMPT_COMMAND="history -a" # write memory to file on every command entered
+PROMPT_COMMAND="history -a" # write memory to history file on every command entered
 
 ##### We work with #####
 export PAGER="less"
