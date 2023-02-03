1b12c6673752d0083936bc4395ad6fbb6906d88f
Hermann Stanew <hermann-stanew@invita.gmbh>
2023-02-03 07:54:03 +0000

    fix shell globbing

--- home/invita.rc.bash
+++ home/invita.rc.bash
@@ -5,14 +5,9 @@ trap 'echo "Goodbye $USER, now lvl $((SHLVL-1))"' EXIT
 # don't cache binary locations. search $PATH on every command entered
 set +h
 
-# send ANSI color codes to the terminal
-# # show me someone with a b&w CRT monitor
-# # nvm: https://www.reddit.com/r/crtgaming/comments/u2nbu4/may_i_present_you_this_tiny_bw_crt_its_only_5/
-alias ls='ls --color=auto -h'
-alias grep='grep --color=auto'
-
-# Enable duplicate `**` glob expansion
-shopt -s globstar
+# globbing exists, it should at least work
+shopt -s nullglob globstar dotglob
+GLOBIGNORE=.:..
 
 # tab tab tab
 source /usr/share/bash-completion/bash_completion
@@ -20,10 +15,21 @@ source /usr/share/bash-completion/bash_completion
 ##### History not Mystery #####
 # https://manpages.debian.org/bullseye/bash/bash.1.en.html#HISTORY
 
-HISTIGNORE="export *" # but keep secrets secret
-HISTSIZE=-1 # keep history of all commands (not just 500) entered
-shopt -s histappend # append to the history file, don't overwrite it
-PROMPT_COMMAND="history -a" # write memory to history file on every command entered
+# but keep secrets secret
+HISTIGNORE="export *"
+# keep history of all commands (not just 500) entered
+HISTSIZE=-1
+# append to the history file, don't overwrite it
+shopt -s histappend
+# write memory to history file on every command entered
+PROMPT_COMMAND="history -a"
+
+##### Commands #####
+# send ANSI color codes to the terminal
+# # show me someone with a b&w CRT monitor
+# # nvm: https://www.reddit.com/r/crtgaming/comments/u2nbu4/may_i_present_you_this_tiny_bw_crt_its_only_5/
+alias ls='ls --color=auto -h'
+alias grep='grep --color=auto'
 
 ##### We work with #####
 export PAGER="less"
