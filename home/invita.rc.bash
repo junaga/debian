1af9ba86bfbf525793fb970366f517999685374b
Hermann Stanew <hermann-stanew@invita.gmbh>
2023-02-04 11:55:05 +0000

    drop opinionated `ls -h` alias

--- home/invita.rc.bash
+++ home/invita.rc.bash
@@ -14,20 +14,15 @@ source /usr/share/bash-completion/bash_completion
 
 ##### History not Mystery #####
 # https://manpages.debian.org/bullseye/bash/bash.1.en.html#HISTORY
-
-# but keep secrets secret
-HISTIGNORE="export *"
-# keep history of all commands (not just 500) entered
-HISTSIZE=-1
-# append to the history file, don't overwrite it
-shopt -s histappend
-# write memory to history file on every command entered
-PROMPT_COMMAND="history -a"
+HISTIGNORE="export *" # but keep secrets secret
+HISTSIZE=-1 # keep history of all commands (not just 500) entered
+shopt -s histappend # append to the history file, don't overwrite it
+PROMPT_COMMAND="history -a" # write memory to history file on every command entered
 
 ##### Commands #####
 # send ANSI color codes to the terminal
 # # show me someone with a b&w CRT monitor
 # # nvm: https://www.reddit.com/r/crtgaming/comments/u2nbu4/may_i_present_you_this_tiny_bw_crt_its_only_5/
-alias ls='ls --color=auto -h'
+alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias diff='diff --color=auto'
