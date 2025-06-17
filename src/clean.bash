4a89057e2ab7c2faa27599eee6582225b0f4c1e2
junaga <hermann-stanew@invita.gmbh>
2025-06-17 23:13:05 +0200

    history, what else remains..

--- src/clean.bash
+++ src/clean.bash
@@ -6,6 +6,6 @@ rm -r ~/.vscode-server
 rm ~/.sudo_as_admin_successful
 rm ~/.wget-hsts
 
-rm ~/.bash_history
-rm ~/.python_history
-rm ~/.node_repl_history
+# rm ~/.bash_history
+# rm ~/.python_history
+# rm ~/.node_repl_history
