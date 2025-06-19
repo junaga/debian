639ccfcb8f713e2aa23e65e761f7e73ad46b6a45
junaga <hermann-stanew@invita.gmbh>
2025-06-19 19:07:32 +0200

    stash wsl docs

--- home/junaga.bash
+++ home/junaga.bash
@@ -1,6 +1,6 @@
 export PAGER="/bin/more"
 export SHELL="/bin/bash"
-export BROWSER="/bin/echo BROWSER: " # brave-browser-stable
+export BROWSER="explorer.exe" # brave-browser-stable
 
 export EMAIL="hermann-stanew@invita.gmbh"
 export EDITOR="code --wait -" # https://code.visualstudio.com/
