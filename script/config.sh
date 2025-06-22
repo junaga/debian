7db8d170987e63d91fe38fec5d7b8ce5d7e87083
junaga <hermann-stanew@invita.gmbh>
2025-06-22 07:22:22 +0200

    uhhh

--- script/config.sh
+++ script/config.sh
@@ -1,7 +1,6 @@
 git config --global user.name $USER
 git config --global user.email $EMAIL
 git config --global init.defaultBranch main
-git config --global alias.glog "log --all --graph --date=short --pretty=format:'%C(yellow)%h %C(white)%an %ad %s %C(auto) %d %Creset'"
 
 gh auth login \
   --git-protocol https \
