a66c23f1f0b9eaef9459ca1f29ab27e6e55f2042
junaga <hermann@stanew.name>
2026-03-11 18:34:38 +0100

    vscode settings and extensions

---
+++ desktop/editor/extensions.sh
@@ -0,0 +1,12 @@
+# VS Code Extensions
+
+alias install="code --install-extension"
+
+install eric-blanquer.ansi-automatic-clean-viewer # https://marketplace.visualstudio.com/items?itemName=eric-blanquer.ansi-automatic-clean-viewer
+install esbenp.prettier-vscode # https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+install grapecity.gc-excelviewer # https://marketplace.visualstudio.com/items?itemName=grapecity.gc-excelviewer
+install leonardssh.vscord # https://marketplace.visualstudio.com/items?itemName=leonardssh.vscord
+install ozaki.markdown-github-dark # https://marketplace.visualstudio.com/items?itemName=ozaki.markdown-github-dark
+install pkief.material-icon-theme # https://marketplace.visualstudio.com/items?itemName=pkief.material-icon-theme
+install vue.volar # https://marketplace.visualstudio.com/items?itemName=vue.volar
+install wix.vscode-import-cost # https://marketplace.visualstudio.com/items?itemName=wix.vscode-import-cost
