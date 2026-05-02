cfc3b4d8ab2307d2460b52ba18311a83b2b4f5e1
junaga <hermann@stanew.name>
2026-05-02 15:08:30 +0000

    fix extensions; they are listed different if on client or server

--- desktop/vscode/extensions.sh
+++ desktop/vscode/extensions.sh
@@ -1,9 +1,11 @@
 alias inst="code --install-extension"
 
+inst qwtel.sqlite-viewer # https://marketplace.visualstudio.com/items?itemName=qwtel.sqlite-viewer
 inst ms-vscode-remote.remote-ssh # https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-ssh
-inst pkief.material-icon-theme # https://marketplace.visualstudio.com/items?itemName=pkief.material-icon-theme
+inst PKief.material-icon-theme # https://marketplace.visualstudio.com/items?itemName=PKief.material-icon-theme
 inst ozaki.markdown-github-dark # https://marketplace.visualstudio.com/items?itemName=ozaki.markdown-github-dark
-inst grapecity.gc-excelviewer # https://marketplace.visualstudio.com/items?itemName=grapecity.gc-excelviewer
+inst GrapeCity.gc-excelviewer # https://marketplace.visualstudio.com/items?itemName=GrapeCity.gc-excelviewer
 inst eric-blanquer.ansi-automatic-clean-viewer # https://marketplace.visualstudio.com/items?itemName=eric-blanquer.ansi-automatic-clean-viewer
-inst vue.volar # https://marketplace.visualstudio.com/items?itemName=vue.volar
-inst leonardssh.vscord # https://marketplace.visualstudio.com/items?itemName=leonardssh.vscord
+inst esbenp.prettier-vscode # https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+inst Vue.volar # https://marketplace.visualstudio.com/items?itemName=Vue.volar
+inst LeonardSSH.vscord # https://marketplace.visualstudio.com/items?itemName=LeonardSSH.vscord
