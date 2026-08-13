b1f10bb0ceb7449073c45ec6baddec409bc44bd0
junaga <hermann@stanew.name>
2026-08-14 00:38:32 +0200

    Configure desktop networking

--- base/setup.sh
+++ base/setup.sh
@@ -12,26 +12,3 @@ cp -ar "$DIR/etc/apt/." /etc/apt/.
 
 # run upgrades at boot
 echo '@reboot /bin/bash /usr/local/src/base/upgrade.sh' | crontab -
-
-# migrate to NetworkManager
-apt install --yes network-manager
-crudini --set /etc/NetworkManager/NetworkManager.conf ifupdown managed true
-nmcli connection migrate
-systemctl restart NetworkManager
-crudini --set /etc/NetworkManager/NetworkManager.conf main plugins keyfile
-crudini --set /etc/NetworkManager/NetworkManager.conf main rc-manager file
-systemctl stop 'ifup@*.service'
-systemctl disable --now networking
-
-# use Cloudflare DNS
-nmcli -t -f UUID,TYPE connection show | while IFS=: read -r uuid type; do
-	case "$type" in
-	802-3-ethernet|802-11-wireless) nmcli connection modify "$uuid" \
-		ipv4.ignore-auto-dns yes ipv6.ignore-auto-dns yes ipv4.dns 1.1.1.1,1.0.0.1 ;;
-	esac
-done
-systemctl restart NetworkManager
-
-# set author for git commits
-git config --global user.name "$USER"
-git config --global user.email "$USER@$HOSTNAME"
--- desktop/install.sh
+++ desktop/install.sh
@@ -60,6 +60,23 @@ apt install --yes\
 # SERVICES
 # ==============================================================================
 
+# NetworkManager with Cloudflare DNS.
+apt install --yes network-manager
+crudini --set /etc/NetworkManager/NetworkManager.conf ifupdown managed true
+nmcli connection migrate
+systemctl restart NetworkManager
+crudini --set /etc/NetworkManager/NetworkManager.conf main plugins keyfile
+crudini --set /etc/NetworkManager/NetworkManager.conf main rc-manager file
+systemctl stop 'ifup@*.service'
+systemctl disable --now networking
+nmcli -t -f UUID,TYPE connection show | while IFS=: read -r uuid type; do
+	case "$type" in
+	802-3-ethernet|802-11-wireless) nmcli connection modify "$uuid" \
+		ipv4.ignore-auto-dns yes ipv6.ignore-auto-dns yes ipv4.dns 1.1.1.1,1.0.0.1 ;;
+	esac
+done
+systemctl restart NetworkManager
+
 # Audio and Bluetooth
 apt install --yes\
   pipewire-audio\
