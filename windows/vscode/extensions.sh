5f04bccd93a4a4988c85847e071e826840862b92
junaga <hermann@stanew.name>
2026-09-05 17:54:03 +0200

    Add Live Share to VS Code extension installer

--- windows/vscode/extensions.sh
+++ windows/vscode/extensions.sh
@@ -6,6 +6,7 @@ function inst { code --install-extension "$1"; }
 
 inst ms-vscode-remote.remote-ssh # https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-ssh
 inst ms-vscode-remote.remote-containers # https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-containers
+inst ms-vsliveshare.vsliveshare # https://marketplace.visualstudio.com/items?itemName=ms-vsliveshare.vsliveshare
 inst PKief.material-icon-theme # https://marketplace.visualstudio.com/items?itemName=PKief.material-icon-theme
 inst ozaki.markdown-github-dark # https://marketplace.visualstudio.com/items?itemName=ozaki.markdown-github-dark
 inst Vue.volar # https://marketplace.visualstudio.com/items?itemName=Vue.volar
